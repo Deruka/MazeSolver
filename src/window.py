ccf744b8edfae6b640662fa004d30f77f1267f5c
Deruka <deruka.km@googlemail.com>
2025-02-11 14:13:06 +0100

    added the rest for the window class and added it to the main.py to create a window that stays open until closed, added gitignore for pycache

--- src/window.py
+++ src/window.py
@@ -2,11 +2,24 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
         self.__root = Tk()
         self.__root.title("Maze Solver")
-        self.__canvas = Canvas(master=self.__root)
+        self.__canvas = Canvas(master=self.__root, width=self.__width, height= self.__height)
         self.__canvas.pack(fill=BOTH)
         self.__winactive = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__winactive = True
+        while self.__winactive:
+            self.redraw()
+
+    def close(self):
+        self.__winactive = False
+    
